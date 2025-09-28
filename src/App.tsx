9077f2ddea286a133967bab8862b16128c67e534
ulvi986 <64612489+ulvi986@users.noreply.github.com>
2025-09-28 20:45:42 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Layout } from "./components/Layout";
 import Index from "./pages/Index";
 import History from "./pages/History";
-import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
+import Profile from "./components/Profile";
+import { PaymentDashboard  } from "./components/PaymentDashboard";
 
 const queryClient = new QueryClient();
 
@@ -17,15 +17,14 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/history" element={<History />} />
-            <Route path="/profile" element={<Profile />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Layout>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/payment-dashboard" element={<PaymentDashboard />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
